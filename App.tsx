4d7b9f42377ce8b4996d8403d86ac5902a8cb7ae
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-06-06 23:49:19 -0300

    fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+import { ActivityIndicator } from 'react-native';
+
 import Group from './src/Screens/Group';
 import {ThemeProvider} from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import theme from './src/themes'
+import theme from './src/theme'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Group />
+      { fontsLoaded ? <Group /> : <ActivityIndicator /> }
     </ThemeProvider>  
   );
 }
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,5 +1,5 @@
-import 'styled-components'
-import theme from '../themes'
+import 'styled-components/native'
+import theme from '../theme'
 
 declare module 'styled-components' {
     type ThemeType = typeof theme
