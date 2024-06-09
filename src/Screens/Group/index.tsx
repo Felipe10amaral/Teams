b818607125cfeef04f7da040dafb3f0f23a001a3
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-06-09 00:49:36 -0300

    biblioteca de icones

--- src/Screens/Group/index.tsx
+++ src/Screens/Group/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from '../../Components/Header';
 import { Container } from './styles';
 
 export default function Group() {
   return (
     <Container>
-     
+      <Header showBackButton/>
+      
      
     </Container>
   );
