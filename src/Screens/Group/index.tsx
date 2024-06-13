d8b46126562c719213cc265256cd95bbff871471
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-06-12 23:31:29 -0300

    listando grupos

--- src/Screens/Group/index.tsx
+++ src/Screens/Group/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import {FlatList} from 'react-native'
 import { GroupCard } from '../../Components/GroupCard';
 import { Header } from '../../Components/Header';
 import { Highlight } from '../../Components/Highlight'
 import { Container } from './styles';
 
 export default function Group() {
+  const [groups, setGroups] = useState<string[]>(['tufão', 'tamires'])
+
   return (
     <Container>
       <Header />
@@ -13,7 +17,15 @@ export default function Group() {
         subTitle='Jogue com seus amigos'
       />
 
-      <GroupCard title='Galera da ITEC' />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={ ({item}) => (
+          <GroupCard title={item} />
+        )}
+      />
+
+      
      
     </Container>
   );
